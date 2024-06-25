647aed79c5f6d007e01d5572986f587327d522d8
Sam Brunkard <sbrunkard@protonmail.com>
2024-06-25 15:56:29 +0100

    Updated with docstrings

--- src/remove_sensitive.py
+++ src/remove_sensitive.py
@@ -1,15 +1,14 @@
+"""Removes name and card number"""
 import extract as ex
 
 leeds_info = ex.reading_leeds_csv_files()
 chesterfield_info = ex.reading_chesterfield_csv_files()
 
 def remove_pii(dataset):
+    """Removes pii from each dataset passed in"""
     expunged_data = []
     for row in dataset:
         row.pop(2)
         row.pop(5)
         expunged_data.append(row)
     return expunged_data
-
-
-
