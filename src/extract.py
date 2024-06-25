06783fd83ded8efacd2032c5c03b3443af9efa85
Sam Brunkard <sbrunkard@protonmail.com>
2024-06-25 14:03:21 +0100

    Updated extract.py

--- src/extract.py
+++ src/extract.py
@@ -1,11 +1,19 @@
+"""The code for reading the CSV file"""
 import csv
 
 def reading_csv_files():
+    "Reads CSV file from data folder"
     try:
-        with open("data/leeds.csv", 'r') as file:
-            csv_file = csv.DictReader(file)
-            products_list = list(csv_file)
+        with open("data/leeds.csv", 'r', encoding="UTF-8") as file:
+            csv_file = csv.reader(file)
+            orders_list = []
+            for row in csv_file:
+                orders_list.append(row)
     except FileNotFoundError:
-        leeds_list = []
+        print("File not found")
+        orders_list = []
+    return orders_list
 
-reading_csv_files()
+leeds_orders_list = reading_csv_files()
+for rows in leeds_orders_list:
+    print(rows)
