9bbb2e32ada8cf65836752202e2fb230dee7d291
Marko <mara55728@gmail.com>
2024-06-27 16:24:21 +0100

    Refractored code

--- src/item_separation.py
+++ src/item_separation.py
@@ -1,5 +1,6 @@
 import extract as ex
 import remove_sensitive as rm
+import generate_uids as uids  # Import the uids module to access hash_ids_list function
 
 """This is separating each item name and price in each order into a list of dictionaries from data folder."""
 def item_separation():
@@ -11,19 +12,26 @@ def item_separation():
         all_data = rm.remove_pii(data)
         
         result = [
-            ["item_id", "item_name", "price"]
+            ["order_id", "item_name", "price"]
         ]
 
-        for item_id, row in enumerate(all_data):
+        dataset = []
+        for row in all_data:
             if len(row) > 2:  # Ensure the row has the items column
-                items = row[2].split(", ")
-                for item in items:
-                    name, price = item.rsplit(" - ", 1)
-                    result.append([item_id, name.strip(), float(price.strip())])
-        
-        all_results[file_name] = result
+                dataset.append(row)
 
+        ids_list = uids.hash_ids_list(dataset)
+
+        for order_index, row in zip(ids_list, dataset):
+            items = row[2].split(", ")
+            for item in items:
+                name, price = item.rsplit(" - ", 1)
+                result.append([order_index, name.strip(), float(price.strip())])
+
+        all_results[file_name] = result
+    
     return all_results
+
 
 if __name__ == "__main__":
     items = item_separation()
