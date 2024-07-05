3b65c537794ca559251e76173ab7721304f19791
Marko <mara55728@gmail.com>
2024-07-05 09:55:09 +0100

    Cleaned the code

--- src/extract.py
+++ src/extract.py
@@ -1,17 +1,3 @@
-import csv
-import glob
-
-def reading_all_csv_files(directory="data"):
-    """Reads all CSV files from the specified directory"""
-    all_files_data = {}
-    for filepath in glob.glob(f"{directory}/*.csv"):
-        try:
-            with open(filepath, 'r', encoding="UTF-8") as file:
-                csv_file = csv.reader(file)
-                all_files_data[filepath] = list(csv_file)
-        except FileNotFoundError:
-            print(f"File not found: {filepath}")
-    return all_files_data
 import boto3
 import csv
 import io
